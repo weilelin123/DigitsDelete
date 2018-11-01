bea007d097c42f8ff716d6d1d5d58ea777a7e8f8
weilelin <linweile@hawii.edu>
2018-11-01 09:49:49 -1000

    1

---
+++ app/imports/startup/both/index.js
@@ -0,0 +1,2 @@
+import '/imports/api/stuff';
+import '/imports/api/contact/';
---
+++ app/imports/ui/pages/ListContacts.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container, Loader, Header, Card } from 'semantic-ui-react';
+import { Contacts } from '/imports/api/contact/contact';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import Contact from '/imports/ui/components/Contact';
+
+/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+class ListContacts extends React.Component {
+
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  /** Render the page once subscriptions have been received. */
+  renderPage() {
+    return (
+        <Container>
+          <Header as="h2" textAlign="center" inverted>List Contacts</Header>
+          <Card.Group>
+            {this.props.contacts.map((contact,index) => <Contact key={index} contact={contact} />)}
+          </Card.Group>
+        </Container>
+    );
+  }
+}
+
+/** Require an array of Stuff documents in the props. */
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+/** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe('Contacts');
+  return {
+    contacts: Contacts.find({}).fetch(),
+    ready: subscription.ready(),
+  };
+})(ListContacts);
